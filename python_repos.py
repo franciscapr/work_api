7b24640990e88534dd7a0695f146447080901497
Francisca <francisca.paredes.pr@gmail.com>
2025-02-21 12:26:20 -0300

    diccionario de respuesta

--- python_repos.py
+++ python_repos.py
@@ -3,7 +3,7 @@ import requests
 # Realiza una llamada a la API y verifica la respuesta.
 
 url = "http://api.github.com/search/repositories"
-url += "?q=language:python+sort:stars-stars:>10000"
+url += "?q=language:python+sort:stars+stars:>10000"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
@@ -23,7 +23,17 @@ print(f"Repositories returned: {len(repo_dicts)}")
 
 # Examina el primer repositorio.
 repo_dict = repo_dicts[0]
+
+print("\nSekected information about first repository:")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
+
 print(f"\nKeys: {len(repo_dict)}")
 
-for key in sorted(repo_dict.keys()):
-    print(key)
+#for key in sorted(repo_dict.keys()):
+#    print(key)
