55f55144da6bca5802b197ea283adf7fd9865f0d
Francisca <francisca.paredes.pr@gmail.com>
2025-02-21 12:14:12 -0300

    diccionario de respuesta

--- python_repos.py
+++ python_repos.py
@@ -13,5 +13,17 @@ print(f"Status code: {r.status_code}")
 # Convierte el objeto de respuesta en una diccionario.
 response_dict = r.json()
 
-# Procesa los resulados.
-print(response_dict.keys())
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
+
+# Explora la informaciòn sobre los repositorios.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+
+# Examina el primer repositorio.
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+
+for key in sorted(repo_dict.keys()):
+    print(key)
