dc1de518899993f1383938d7e683ee6a690a66bc
Francisca <francisca.paredes.pr@gmail.com>
2025-02-21 15:22:09 -0300

    añadir enlaces activo a nuestro grafico

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -18,9 +18,16 @@ print(f"Total repositories: {response_dict['total_count']}")
 
 # Explora la informaciòn sobre los repositorios.
 repo_dicts = response_dict['items']
+repo_links, stars, labels = [], [], []
 repo_names, stars, hover_texts = [], [], []
 
 for repo_dict in repo_dicts:
+    # Convierte los nombres del repositorio en enlaces activos.
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+  
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
 
@@ -34,7 +41,7 @@ for repo_dict in repo_dicts:
 # Crea la visualizaciòn.
 title = "Most-Starred Python Project on GitHub"
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels, hover_name=hover_texts)
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts)
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
 fig.show()
 
