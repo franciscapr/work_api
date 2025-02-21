9b81e40c61ac468ff35f735fb5f23d6e60728f8b
Francisca <francisca.paredes.pr@gmail.com>
2025-02-21 12:34:27 -0300

    resumir los principales repositorios

--- python_repos.py
+++ python_repos.py
@@ -22,18 +22,30 @@ print(f"Repositories returned: {len(repo_dicts)}")
 
 
 # Examina el primer repositorio.
-repo_dict = repo_dicts[0]
+# repo_dict = repo_dicts[0]
 
-print("\nSekected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+# print("\nSelected information about first repository:")
+# print(f"Name: {repo_dict['name']}")
+# print(f"Owner: {repo_dict['owner']['login']}")
+# print(f"Stars: {repo_dict['stargazers_count']}")
+# print(f"Repository: {repo_dict['html_url']}")
+# print(f"Created: {repo_dict['created_at']}")
+# print(f"Updated: {repo_dict['updated_at']}")
+# print(f"Description: {repo_dict['description']}")
 
-print(f"\nKeys: {len(repo_dict)}")
+# print(f"\nKeys: {len(repo_dict)}")
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    
+    
 
 #for key in sorted(repo_dict.keys()):
 #    print(key)
