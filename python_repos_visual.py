6ffe2a9720bc3aa185e389a7f776cc5936dbac35
Francisca <francisca.paredes.pr@gmail.com>
2025-02-21 12:45:59 -0300

    visualizar repositorios con plotly

---
+++ python_repos_visual.py
@@ -0,0 +1,63 @@
+import requests
+import plotly.express as px
+
+# Realiza una llamada a la API y verifica la respuesta.
+
+url = "http://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Convierte el objeto de respuesta en una diccionario.
+response_dict = r.json()
+print(f"Complete results: {not response_dict['incomplete_results']}")
+print(f"Total repositories: {response_dict['total_count']}")
+
+
+# Explora la informaciòn sobre los repositorios.
+repo_dicts = response_dict['items']
+repo_names, stars = [], []
+
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+
+# Crea la visualizaciòn.
+fig = px.bar(x=repo_names, y=stars)
+fig.show()
+
+
+# print(f"Repositories returned: {len(repo_dicts)}")
+
+
+# Examina el primer repositorio.
+# repo_dict = repo_dicts[0]
+
+# print("\nSelected information about first repository:")
+# print(f"Name: {repo_dict['name']}")
+# print(f"Owner: {repo_dict['owner']['login']}")
+# print(f"Stars: {repo_dict['stargazers_count']}")
+# print(f"Repository: {repo_dict['html_url']}")
+# print(f"Created: {repo_dict['created_at']}")
+# print(f"Updated: {repo_dict['updated_at']}")
+# print(f"Description: {repo_dict['description']}")
+
+# print(f"\nKeys: {len(repo_dict)}")
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    
+    
+
+#for key in sorted(repo_dict.keys()):
+#    print(key)
